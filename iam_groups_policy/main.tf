c1b09c432a7e5f4460bac2cabac9254671ef921b
Adelson Junior <adesjnr@Guarau.local>
2017-03-17 17:48:35 -0300

    Fix in tab

--- iam_groups_policy/main.tf
+++ iam_groups_policy/main.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_group_policy" "iam_group_policy" {
   name  = "${var.name}"
   group = "${var.group_id}"
-	policy = ${file("${var.policy_file}")}
+  policy = ${file("${var.policy_file}")}
 }
